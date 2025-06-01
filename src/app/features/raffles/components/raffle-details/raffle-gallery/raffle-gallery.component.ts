8ab15e892a8ac63c19886c2ece1fe2ee577a777d
miguel-kultur <miguel@appkultur.com>
2025-06-01 16:02:43 +0200

    Fix thumbnails behaviour in raffle details page gallery

--- src/app/features/raffles/components/raffle-details/raffle-gallery/raffle-gallery.component.ts
+++ src/app/features/raffles/components/raffle-details/raffle-gallery/raffle-gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 import { Image } from '../../../models/image.model';
 
 @Component({
@@ -8,4 +8,18 @@ import { Image } from '../../../models/image.model';
 })
 export class RaffleGalleryComponent {
   @Input() images!: Image[];
+  
+  selectedImageIndex = signal(0);
+
+  get selectedImage(): Image {
+    return this.images[this.selectedImageIndex()] || this.images[0];
+  }
+
+  selectImage(index: number): void {
+    this.selectedImageIndex.set(index);
+  }
+
+  isSelected(index: number): boolean {
+    return this.selectedImageIndex() === index;
+  }
 } 
